b3e6edfbf4d067e0348b99ca95f6334c68f36ad9
太空小孩 <dksnow2002@gmail.com>
2018-05-28 19:14:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Himawari Wallpaper
+
+## 介绍 Introduction
+
+Wallpaper Engine Steam 创意工坊作品。每 10 分钟自动更新来自向日葵八号卫星（Himawari-8）的东亚地区真彩色云图并设置为桌面壁纸。「真彩色云图」为向日葵八号卫星 3 个可见光频道资料叠加所得的影像，由于类似肉眼所见，因此称为「真彩色云图」。
+
+Wallpaper for Wallpaper Engine. Automatically get latest satellite image of East Asia from Himawari-8 and set as wallpaper every 10 minutes. Data in visible bands of red (0.64µm), green (0.51µm) and blue (0.47µm) satellite are combined and Rayleigh scattering modified to produce an RGB product called true-color image, so named because its color is similar to what may appear to the naked eye.
+
+## 下载 Download
+
+* [https://steamcommunity.com/sharedfiles/filedetails/?id=1394885409](https://steamcommunity.com/sharedfiles/filedetails/?id=1394885409)
+
+## 协议 License
+
+[GPL v3](LICENSE)
---
+++ src/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  function setWallpaper() {
+    $("#status").text("Updating image...");
+    // Get time 30 minutes ago in UTC+8 timezone
+    var timezoneLocal = -(new Date().getTimezoneOffset() / 60),
+      timezoneDest = 8,
+      offset = timezoneDest - timezoneLocal;
+    console.log("Local Timezone: " + timezoneLocal);
+    console.log("Offset: " + offset);
+    var delay = 30000 * 60;
+    var date = new Date(new Date().getTime() + offset * 3600 * 1000 - delay);
+    console.log("Date in UTC+8: " + date);
+    // Format time
+    var yyyy = date.getFullYear(),
+      MM = ("0" + (date.getMonth() + 1)).slice(-2),
+      dd = ("0" + date.getDate()).slice(-2),
+      hh = ("0" + date.getHours()).slice(-2),
+      mm = ("0" + (Math.floor(date.getMinutes() / 10) * 10)).slice(-2);
+    console.log("Formatted Date in UTC+8: " + yyyy + "-" + MM + "-" + dd + " " + hh + ":" + mm);
+    // Skip 10:40 AM (No image for this time)
+    if (parseInt(hh) == 10 && parseInt(mm) == 40) {
+      mm = (parseInt(mm) - 10).toString();
+      console.log("Skipped 10:40 AM. Downloading image at " + hh + ":" + mm + ".");
+    };
+    // Get image from CWB and set as background image
+    var url = "https://www.cwb.gov.tw/V7/observe/satellite/Data/ts1p/ts1p-" + yyyy + "-" + MM + "-" + dd + "-" + hh + "-" + mm + ".jpg";
+    $("<img/>").attr("src", url).on("load", function() {
+      $(this).remove();
+      $("#wallpaper").css("background-image", "url(" + url + ")");
+      $("#wallpaper").css("opacity", 1);
+      $("#status").text(yyyy + "-" + MM + "-" + dd + " " + hh + ":" + mm + " UTC+8");
+    });
+    console.log("URL: " + url);
+  }
+  setWallpaper();
+  setInterval(setWallpaper, 60000);
+});
